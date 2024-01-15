c5f293250b8a497673b1a0875f2055adce48d21b
birongliu <birongliu2003@gmail.com>
2024-01-15 18:50:45 -0500

    Add DISCORD_CALLBACK_URL to environment schema

--- src/util/validate.schema.ts
+++ src/util/validate.schema.ts
@@ -4,4 +4,5 @@ export const validateEnvironmentSchema = z.object({
   DISCORD_CLIENT_ID: z.string().min(18),
   DISCORD_CLIENT_SECRET: z.string().min(32),
   SESSION_SECRET: z.string().min(1),
+  DISCORD_CALLBACK_URL: z.string(),
 });
