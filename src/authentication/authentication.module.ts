a67def55b27792edb873e9093b48289b32d4e11e
birongliu <birongliu2003@gmail.com>
2024-02-04 19:01:09 -0500

    Update authentication module imports and providers

--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
 import AuthenticationController from './authentication.controller';
 import AuthenticationService from './authentication.service';
-import DiscordStrategy from './Strategies/DiscordStrategy';
+import DiscordStrategy from './strategies/DiscordStrategy';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import MicroservicesModule from 'src/microservices/microservice.module';
+import { SessionSerializer } from './strategies/SessionSerializer';
+import { JwtStrategy } from './strategies/JwtStrategy';
+import GatewayContainer from './gateways/GatewayContainer';
+
 @Module({
-  imports: [],
+  imports: [
+    MicroservicesModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get('JWT_SECRET'),
+      }),
+    }),
+  ],
   controllers: [AuthenticationController],
-  providers: [AuthenticationService, DiscordStrategy],
+  providers: [AuthenticationService, DiscordStrategy, JwtStrategy, SessionSerializer, GatewayContainer],
+  exports: [AuthenticationService, GatewayContainer],
 })
 export class AuthenticationModule {}
+
