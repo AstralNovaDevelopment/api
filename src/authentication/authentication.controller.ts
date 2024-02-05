0fb4537a685f12b88ef53ab1a567ff24b7a014c1
birongliu <birongliu2003@gmail.com>
2024-02-04 19:01:01 -0500

    Refactor authentication controller and add new endpoints

--- src/authentication/authentication.controller.ts
+++ src/authentication/authentication.controller.ts
@@ -1,13 +1,24 @@
-import { Controller, Get, Inject, Res, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Inject,
+  Logger,
+  Post,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import AuthenticationService from './authentication.service';
-import DiscordGuard from './Guards/DiscordGuard';
-import { Response } from 'express';
+import DiscordGuard from './guards/DiscordGuard';
+import { Request, Response } from 'express';
+import AuthenticatedGuard from './guards/AuthenticatedGuard';
+import { ValidateJWTCodeGuard } from './guards/JWTGuard';
+
 
 @Controller('/api/authentication')
 export default class AuthenticationController {
-  constructor(
-    @Inject(AuthenticationService) public auth: AuthenticationService,
-  ) {}
+  constructor(@Inject(AuthenticationService) private auth: AuthenticationService) {}
 
   @UseGuards(DiscordGuard)
   @Get('/discord/login')
@@ -17,7 +28,26 @@ export default class AuthenticationController {
 
   @UseGuards(DiscordGuard)
   @Get('/discord/authorize')
-  public getAuth(@Res() res: Response) {
-    return res.redirect('/api/discord/@me');
+  public async getAuth(@Res() res: Response, @Req() req: Request) {
+    return res.redirect(`zenflow://auth?code=${req.user["tokenId"]}`)
+  }
+
+  @UseGuards(AuthenticatedGuard)
+  @Get("/logout")
+  public async logout(@Req() req: Request, res: Response) {
+    req.logOut({ keepSessionInfo: false }, (err) => Logger.error(err))
+    return res.status(200).json({ status: 200, message: "success" })
+  }
+
+  @Post('/token')
+  @UseGuards(ValidateJWTCodeGuard)
+  public async getToken(@Body() body: Record<string, string>) {
+    return this.auth.getToken(body.code)
+  }
+
+  @Post("/token/revoke")
+  @UseGuards(ValidateJWTCodeGuard)
+  public async revokeToken(@Body() body: Record<string, string>) {
+    return this.auth.revokeToken(body.code)
   }
 }
