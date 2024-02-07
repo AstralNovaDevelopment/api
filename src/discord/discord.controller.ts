7361d9f67f6c0446c61a35e0bd55b61108835782
birongliu <birongliu2003@gmail.com>
2024-02-07 08:57:44 -0500

    Refactor DiscordController imports and remove unused code

--- src/discord/discord.controller.ts
+++ src/discord/discord.controller.ts
@@ -1,11 +1,8 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { Role, User } from '@prisma/client';
+import { User } from '@prisma/client';
 import { Request } from 'express';
 import AuthenticatedGuard from '../authentication/guards/authenticated.guard';
 import JwtAuthGuard from '../authentication/guards/jwt.guard';
-import { Roles } from 'src/authentication/decorator/role.decorator';
-import RoleGuard from 'src/authentication/guards/role.guard';
-
 @Controller('/api/discord')
 export default class DiscordController {
   @UseGuards(JwtAuthGuard, AuthenticatedGuard)
@@ -20,11 +17,5 @@ export default class DiscordController {
       fetchAt: user.fetchedAt,
       role: user.role
     };
-  }
-  @UseGuards(JwtAuthGuard, RoleGuard)
-  @Roles(Role.MEMBER)
-  @Get("/test")
-  test() {
-    return "hello"
   }
 }
